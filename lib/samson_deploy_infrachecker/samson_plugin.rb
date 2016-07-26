397424afefb9a69961221b4044c2a9785ac2449f
yufei.chen <yufei.chen@redbubble.com>
2016-07-26 11:10:52 +1000

    adding two new attributes to permitted params in project controller

--- lib/samson_deploy_infrachecker/samson_plugin.rb
+++ lib/samson_deploy_infrachecker/samson_plugin.rb
@@ -6,6 +6,10 @@ end
 
 Samson::Hooks.view :project_form, "samson_deploy_infrachecker/fields"
 
+Samson::Hooks.callback :project_permitted_params do
+  [:check_infraspec_before_autodeploy, :buildkite_api_token]
+end
+
 Samson::Hooks.callback :release_deploy_conditions do |_, release|
   DeployInfrachecker.new.check_build_status(release.project)
 end
